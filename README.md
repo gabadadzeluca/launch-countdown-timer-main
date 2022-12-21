74d2ee33d93df74715b72373b7681cd19eca7fed
Luca <gabadadzeluca@gmail.com>
2022-12-21 23:01:12 +0400

    finish project

---
+++ README.md
@@ -0,0 +1,64 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for all interactive elements on the page
+- See a live countdown timer that ticks down every second (start the count at 14 days)
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/gabadadzeluca/launch-countdown-timer-main)
+- Live Site URL: [](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+
+### What I learned
+
+I learned about Date Object and its use in JavaScript.
+Also I've styled and added elements to the page using the help of js.
+I've used setInterval method for the constant change of value on the page. 
+
+
+### Continued development
+
+I wasn't fully able to split the divs into half, so the first and second half don't fully show but they coordinate as if it's one div.
+
+
+### Useful resources
+
+- [How to create costum timer](https://www.w3schools.com/howto/howto_js_countdown.asp) - This helped me understand Date Object and its methods in js, also converting units, which I didn't do myself.
+
+## Author
+
+- LinkedIn - [Luca Gabadadze](https://www.linkedin.com/in/luca-gabadadze-6068b324a/)
+
--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 "use strict";
-const daysDiv = document.querySelector('#days div');
-const hoursDiv = document.querySelector('#hours div');
-const minutesDiv = document.querySelector('#minutes div');
-const secondsDiv = document.querySelector('#seconds div');
 
+const daysDiv = document.querySelector('#days div div');
+const hoursDiv = document.querySelector('#hours div>div');
+const minutesDiv = document.querySelector('#minutes div>div');
+const secondsDiv = document.querySelector('#seconds div>div');
+
+
+// adding decoration to the divs
+const flipCards = document.querySelectorAll('.flip-card');
+
+
+[...flipCards].forEach(div=>{
+    const leftBall = document.createElement('div');
+    leftBall.classList.add('ball');
+    leftBall.classList.add('left');
+
+    const rightBall = document.createElement('div');
+    rightBall.classList.add('ball');
+    rightBall.classList.add('right');
+    console.log(div);
+    div.appendChild(leftBall);
+    div.appendChild(rightBall);
+});
+
+
+const finalDate = new Date("January 1, 2023 00:00:00");
 // const finalDate = new Date("February 1, 2023 00:01:00");
-const finalDate = new Date("January 1, 2023 00:01:00");
 
-function restart(){
-    let x = setInterval(function(){
-        const currentTime = new Date().getTime();
-        let distance = finalDate - currentTime;
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-        daysDiv.innerHTML = days;
-        hoursDiv.innerHTML = hours;
-        minutesDiv.innerHTML = minutes;
-        secondsDiv.innerHTML = seconds;
-    
-        if(distance < 0){
-            clearInterval(x);
-            daysDiv.innerHTML = 0;
-            hoursDiv.innerHTML = 0;
-            minutesDiv.innerHTML = 0;
-            secondsDiv.innerHTML = 0;
-            document.querySelector('header h3').innerHTML = "We've launched!";
-        }
-    
-    }, 1000);   
-}
 
-restart();
+let x = setInterval(function(){
+    const currentTime = new Date().getTime();
+    let distance = finalDate - currentTime;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    daysDiv.innerHTML = days;
+    hoursDiv.innerHTML = hours;
+    minutesDiv.innerHTML = minutes;
+    secondsDiv.innerHTML = seconds;
+
+    if(distance < 0){
+        clearInterval(x);
+        daysDiv.innerHTML = 0;
+        hoursDiv.innerHTML = 0;
+        minutesDiv.innerHTML = 0;
+        secondsDiv.innerHTML = 0;
+        document.querySelector('header h3').innerHTML = "We've launched!";
+    }
+
+}, 1000);   
 
